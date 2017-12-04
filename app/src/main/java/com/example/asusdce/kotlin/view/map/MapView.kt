286b97b099030424703d5bb9f43063f310f27ee3
RockDrigo10 <rodrigo.chavez10@gmail.com>
2017-12-04 17:42:59 -0500

    google maps Location using kotlin

--- app/src/main/java/com/example/asusdce/kotlin/view/map/MapView.kt
+++ app/src/main/java/com/example/asusdce/kotlin/view/map/MapView.kt
@@ -1,27 +1,63 @@
 package com.example.asusdce.kotlin.view.map
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
 import com.example.asusdce.kotlin.R
-
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 
-class MapView : AppCompatActivity(), OnMapReadyCallback {
+class MapView : AppCompatActivity(), OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
-    private lateinit var mMap: GoogleMap
+    private val PERMISSIONS_REQUEST_LOCATION: Int = 10
+    private var mapFragment: SupportMapFragment? = null
+    private var map: GoogleMap? = null
+    private var client: GoogleApiClient? = null
+    private var locationRequest: LocationRequest? = null
+    private var currentLocation: Location? = null
+    private var currentLocationMarker: Marker? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map_view)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
+        mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        mapFragment!!.getMapAsync(this)
+
+        checkPermissions()
+    }
+
+    fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+                        PERMISSIONS_REQUEST_LOCATION)
+            }
+        } else {
+            //getLocation()
+        }
     }
 
     /**
@@ -34,11 +70,82 @@ class MapView : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        map = googleMap
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            buildGoogleApiClient()
+            map!!.isMyLocationEnabled = true
+        }
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    @Synchronized private fun buildGoogleApiClient() {
+        client = GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build()
+        client!!.connect()
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        currentLocation = location
+        if (currentLocationMarker != null) {
+            currentLocationMarker!!.remove()
+        }
+        val latlng = LatLng(location!!.latitude, location!!.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(latlng)
+        markerOptions.title("My Location")
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+        currentLocationMarker = map!!.addMarker(markerOptions)
+        map!!.moveCamera(CameraUpdateFactory.newLatLng(latlng))
+        map!!.animateCamera(CameraUpdateFactory.zoomBy(10f))
+        if (client != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(client,this)
+        }
+    }
+
+    override fun onConnected(p0: Bundle?) {
+
+        locationRequest = LocationRequest()
+        locationRequest!!.interval = 1000
+        locationRequest!!.fastestInterval = 1000
+        locationRequest!!.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(client, locationRequest, this)
+        }
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_LOCATION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        if(client == null)
+                            buildGoogleApiClient()
+                        map!!.isMyLocationEnabled = true
+                    }
+                }else{
+                    Toast.makeText(this,"Permissions granted", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+
+        }
+    }
+
+    companion object {
+        val REQUEST_LOCATION_CODE = 99
     }
 }
